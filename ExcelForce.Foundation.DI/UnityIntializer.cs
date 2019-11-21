c9c3fbfbb6fe5c545228390946bc6d840625fcf7
Ritwik Sen <risen@deloitte.com>
2019-11-21 08:35:09 +0530

    DI initialization

---
+++ ExcelForce.Foundation.DI/UnityIntializer.cs
@@ -0,0 +1,17 @@
+﻿using Unity;
+using Unity.RegistrationByConvention;
+
+namespace ExcelForce.Foundation.DI
+{
+    public static class UnityIntializer
+    {
+        public static void Initialize()
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterTypes(
+                AllClasses.FromAssembliesInBasePath(),
+                (c) => WithMappings.FromMatchingInterface(c));
+        }
+    }
+}
