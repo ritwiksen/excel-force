0f363cfda29c104db97f19c6c5c1d2a694cf835e
Ritwik Sen <risen@deloitte.com>
2019-11-27 18:08:59 +0530

    Added file content manager

---
+++ ExcelForce.Foundation.CoreServices/FileManagement/Interfaces/FileContentManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ExcelForce.Foundation.CoreServices.FileManagement.Interfaces
+{
+    public class FileContentManager : IContentStreamManager
+    {
+        public string ReadContent(string resourceName)
+        {
+            using (StreamReader reader = new StreamReader(resourceName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public bool WriteContent(string resourceName, string content)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(resourceName))
+                {
+                    writer.WriteLine(content);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //TODO:(Ritwik):: Add logging here
+                return false;
+            }
+        }
+    }
+}
---
+++ ExcelForce.Foundation.CoreServices/FileManagement/Interfaces/IContentStreamManager.cs
@@ -0,0 +1,19 @@
+﻿namespace ExcelForce.Foundation.CoreServices.FileManagement.Interfaces
+{
+    public interface IContentStreamManager
+    {
+        /// <summary>
+        /// This method reads content from an external resource
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <returns></returns>
+        string ReadContent(string resourceName);
+
+        /// <summary>
+        /// This method Writes String content to an external resource
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <returns></returns>
+        bool WriteContent(string resourceName, string content);
+    }
+}
