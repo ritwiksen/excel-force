9778619537490f94a254cc9b3a0a2172b8d16c74
Ritwik Sen <risen@deloitte.com>
2020-01-14 15:34:09 +0530

    Added new model

---
+++ ExcelForce.Foundation.EntityManagement/Models/SfEntities/SfDisplayData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExcelForce.Foundation.EntityManagement.Models.SfEntities
+{
+    public class SfDisplayData
+    {
+        public string ItemUrl { get; set; }
+
+        private Hashtable DisplayData { get; set; }
+
+        public void SetData<T>(string key, T value)
+        {
+            DisplayData = DisplayData ?? new Hashtable();
+
+            if (DisplayData.ContainsKey(key))
+            {
+                DisplayData[key] = value;
+            }
+            else
+            {
+                DisplayData.Add(key, value);
+            }
+        }
+
+        public T GetData<T>(string key)
+        {
+            return DisplayData.ContainsKey(key)
+                ? (T)DisplayData[key]
+                : default(T);
+        }
+
+        public IEnumerable<string> GetPropertyNameList()
+        {
+            if (DisplayData == null)
+                yield return null;
+
+            foreach (var key in DisplayData?.Keys)
+            {
+                yield return Convert.ToString(key);
+            }
+        }
+    }
+}
